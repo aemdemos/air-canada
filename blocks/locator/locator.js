b360df1dc4ad4babbcbda66c0fa95cfef0f00cb1
Ben Helleman <bhellema@adobe.com>
2025-04-24 15:52:41 -0400

    add locator

--- blocks/locator/locator.js
+++ blocks/locator/locator.js
@@ -1,13 +1,13 @@
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) {
-      li.append(row.firstElementChild);
-    }
-    ul.append(li);
-  });
-  block.textContent = '';
-  block.append(ul);
+  // const ul = document.createElement('ul');
+  // [...block.children].forEach((row) => {
+  //   const li = document.createElement('li');
+  //   while (row.firstElementChild) {
+  //     li.append(row.firstElementChild);
+  //   }
+  //   ul.append(li);
+  // });
+  // block.textContent = '';
+  // block.append(ul);
 }
