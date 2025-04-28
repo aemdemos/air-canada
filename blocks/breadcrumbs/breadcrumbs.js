87e4b1b0707bc874f43b0e207c3421b6d3243d25
Ben Helleman <bhellema@adobe.com>
2025-04-28 15:56:13 -0400

    breadcrumbs

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,54 +1,10 @@
-const isDesktop = window.matchMedia('(min-width: 900px)');
-
-/**
- * Toggles all nav sections
- * @param {Element} sections The container element
- * @param {Boolean} expanded Whether the element should be expanded or collapsed
- */
-function toggleAllNavSections(locator, expanded = false) {
-  locator.querySelectorAll(':scope > li').forEach((section) => {
-    section.setAttribute('aria-expanded', expanded);
-  });
-}
-
 export default function decorate(block) {
-  const nav = document.createElement('nav');
-  nav.classList.add('nav-sections');
+  const ulEls = block.querySelectorAll(':scope ul');
 
-  const ulEl = block.querySelector(':scope ul');
-  const liEls = ulEl.querySelectorAll(':scope > li');
-
-  liEls.forEach((li) => {
-    li.classList.add('nav-drop');
-    const a = li.querySelector('.button-container a');
-    a.classList.remove('button');
-    li.insertBefore(a, li.firstChild);
-    li.querySelector('.button-container').remove();
-
-    li.addEventListener('mouseover', () => {
-      if (isDesktop.matches) {
-        toggleAllNavSections(nav);
-        li.setAttribute('aria-expanded', 'true');
-      }
-    });
-
-    li.addEventListener('mouseenter', () => {
-      if (isDesktop.matches) {
-        toggleAllNavSections(nav);
-        li.setAttribute('aria-expanded', 'true');
-      }
-    });
-
-    li.addEventListener('mouseleave', (event) => {
-      if (event.target.closest('ul')) return;
-      if (isDesktop.matches) {
-        toggleAllNavSections(nav);
-        li.setAttribute('aria-expanded', 'false');
-      }
-    });
+  ulEls.forEach((ul) => {
+    ul.classList.add('nav-drop');
   });
 
-  nav.append(...liEls);
   block.textContent = '';
-  block.append(nav);
+  block.append(...ulEls);
 }
