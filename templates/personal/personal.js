b60fff049fad517ecef53576ed30cbb41450f86c
Ben Helleman <bhellema@adobe.com>
2025-05-05 18:39:07 -0400

    update hero only

--- templates/personal/personal.js
+++ templates/personal/personal.js
@@ -32,11 +32,14 @@ document.addEventListener('credit-card-selected', (event) => {
 
 // add an event listener for the event aue:ui-select  
 document.addEventListener('aue:ui-select', (event) => {
-  // remove active class from all heros
-  document.querySelectorAll('.hero').forEach(hero => {
-    hero.classList.remove('active');
-  });
+  // update the hero class
+  if (event.target.classList.contains('hero')) {
+    // remove active class from all heros
+    document.querySelectorAll('.hero').forEach(hero => {
+      hero.classList.remove('active');
+    });
 
-  // add active class to the hero that was selected
-  event.target.classList.add('active');
+    // add active class to the hero that was selected
+    event.target.classList.add('active');
+  }
 });
