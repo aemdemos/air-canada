6471bc4f3b9d4ace3c60ea0e6211f2cc562119d6
Ben Helleman <bhellema@adobe.com>
2025-05-05 19:44:40 -0400

    support multiple hero el on import

--- tools/importer/transformers/hero.js
+++ tools/importer/transformers/hero.js
@@ -1,28 +1,39 @@
 /* global WebImporter */
 
 const createHero = (main, document, params) => {
-  const hero = main.querySelector('.ae-header.header-infinite');
+  const page = main.querySelector('#credits-cards-overview-td-personal');
+  const heros = page.querySelectorAll('.ae-header');
 
-  if (hero) {
-    const picImg = hero.querySelector('picture > img');
-    picImg.alt = picImg.alt || 'Hero image';
-
-    const headerEl = hero.querySelector('.header-text');
-    const pillEl = headerEl.querySelector('.red-pill');
-
-    const richText = document.createElement('div');
-    richText.append(headerEl.querySelector('h1'));
-    richText.append(headerEl.querySelector('h2'));
-
-    const cells = [
-      ['Hero'],
-      [picImg],
-      [pillEl],
-      [richText],
-    ];
-
-    const block = WebImporter.DOMUtils.createTable(cells, document);
-    hero.append(block);
+  if (!heros) {
+    return;
   }
+
+  const cells = [['Heros']];
+
+  for (const hero of heros) {
+    if (hero) {
+      const picImg = hero.querySelector('picture > img');
+      picImg.alt = picImg.alt || 'Hero image';
+
+      const headerEl = hero.querySelector('.header-text');
+      const pillEl = headerEl.querySelector('.red-pill');
+
+      const richText = document.createElement('div');
+      richText.append(headerEl.querySelector('h1'));
+      richText.append(headerEl.querySelector('h2'));
+
+      const row = [
+        [picImg],
+        [pillEl],
+        [richText],
+      ];
+
+      cells.push(row);
+      hero.remove();
+    }
+  }
+
+  const block = WebImporter.DOMUtils.createTable(cells, document);
+  page.insertBefore(block, page.firstChild);
 };
 export default createHero;
