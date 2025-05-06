c956e5c10658d2cbeafe3998979993e5db0a0982
Ben Helleman <bhellema@adobe.com>
2025-05-05 23:10:43 -0400

    banner changes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,9 +1,16 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default async function decorate(block) {
   const picture = block.querySelector('picture');
+  const optimizedPic = createOptimizedPicture(picture.querySelector('img').src, picture.querySelector('img').alt, false, [{ width: '750' }]);
+  moveInstrumentation(picture.querySelector('img'), optimizedPic.querySelector('img'));
+  picture.querySelector('img').closest('picture').replaceWith(optimizedPic);
 
   const anchor = block.querySelector('p');
-  // anchor.classList.remove('button-container');
-  // anchor.querySelector('a').classList.remove('button');
+  anchor.classList.remove('button-container');
+  anchor.querySelector('a').classList.remove('button');
+  moveInstrumentation(anchor.querySelector('a'), anchor);
 
   block.innerHTML = '';
   block.append(picture);
