e1beb39e37c2b7011145d47d43ed75b4058a9f26
Ben Helleman <bhellema@adobe.com>
2025-05-06 14:11:51 -0400

    fi-banner

--- blocks/fi-banner/fi-banner.js
+++ blocks/fi-banner/fi-banner.js
@@ -1,1 +1,25 @@
-f
+export default function decorate(block) {
+
+  // deconstruct the block's children so that the first div is in one div, and the rest of the children are in another div
+  const bannerContent = document.createElement('div');
+  bannerContent.classList.add('fi-banner-content');
+
+  // take everything but the image (the first div)
+  bannerContent.append(...block.querySelectorAll(':scope > div:not(:first-child)'));
+
+  // move the second and third divs in banner content into a new div
+  const bannerRow = document.createElement('div');
+  bannerRow.classList.add('fi-banner-row');
+
+  // take everything but the text which is the first div
+  bannerRow.append(...bannerContent.querySelectorAll(':scope > div:not(:first-child)'));
+
+  bannerRow.children[0].classList.add('fi-conditions');
+  bannerRow.children[1].classList.add('fi-cta');
+
+  bannerContent.children[0].classList.add('fi-title');
+
+  bannerContent.append(bannerRow);
+  block.append(bannerContent);
+}
+
