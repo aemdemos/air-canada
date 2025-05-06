79d50d175234a5ff1e93e7fee41d8a842e7e3543
Ben Helleman <bhellema@adobe.com>
2025-05-05 23:07:46 -0400

    banner changes

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,8 +1,9 @@
 export default async function decorate(block) {
   const picture = block.querySelector('picture');
+
   const anchor = block.querySelector('p');
-  anchor.classList.remove('button-container');
-  anchor.querySelector('a').classList.remove('button');
+  // anchor.classList.remove('button-container');
+  // anchor.querySelector('a').classList.remove('button');
 
   block.innerHTML = '';
   block.append(picture);
