f24f64c06e162c2e239708bb7c6d9925d55f636b
Ben Helleman <bhellema@adobe.com>
2025-05-07 12:17:12 -0400

    two column

--- blocks/two-columns/two-columns.js
+++ blocks/two-columns/two-columns.js
@@ -5,7 +5,7 @@ export default function decorate(block) {
 
   const container = document.createElement('ul');
   container.classList.add('two-columns-container');
-
+  container.style.setProperty('column-count', cards.length > 1 ? '2' : '1');
   cards.forEach((card) => {
     const li = document.createElement('li');
     moveInstrumentation(card, li);
