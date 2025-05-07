9289e92a6521dc07d52ee9283dbbd77ba85dd100
Ben Helleman <bhellema@adobe.com>
2025-05-07 13:25:02 -0400

    card details

--- blocks/card-details/card-details.js
+++ blocks/card-details/card-details.js
@@ -1,14 +1,21 @@
 export default function decorate(block) {
-  const [title, ...cards] = block.children;
-
+  let [title, ...rest] = block.children;
   const container = document.createElement('div');
   container.classList.add('card-details-container');
+
+  // filter out rest of the children where the children length is 2 and remove them from the rest array
+  let cards = rest.filter((child) => child.children.length === 2);
+  rest = rest.filter((child) => child.children.length !== 2);
 
   cards.forEach((card) => {
     card.classList.add('card-detail');
     container.append(card);
   });
 
+  const eligibility = document.createElement('div');
+  eligibility.classList.add('card-eligibility');
+  eligibility.append(...rest[0].children);
+
   block.innerHTML = '';
-  block.append(title, container);
+  block.append(title, container, eligibility);
 }
