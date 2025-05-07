893cd7022af16816a04b8c4efa210b5caec69415
Ben Helleman <bhellema@adobe.com>
2025-05-07 14:15:38 -0400

    accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -7,14 +7,17 @@ export default function decorate(block) {
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
-    moveInstrumentation(label, summary);
+
     // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
     // decorate accordion item
     const details = document.createElement('details');
+
     details.className = 'accordion-item';
     details.append(summary, body);
+
+    moveInstrumentation(row, details);
     row.replaceWith(details);
   });
 }
