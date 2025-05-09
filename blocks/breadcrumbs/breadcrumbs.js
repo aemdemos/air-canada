ef212fbab6401140a8c1938f9e664edc118290b6
Ben Helleman <bhellema@adobe.com>
2025-05-09 10:42:53 -0400

    breadcrumbs

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,21 +1,21 @@
 /* global WebImporter */
-import { moveInstrumentation } from '../../scripts/scripts.js';
+// import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const ulEls = block.querySelectorAll(':scope ul');
+  // const ulEls = block.querySelectorAll(':scope ul');
 
-  ulEls.forEach((ul, index) => {
-    // move the instrumentation to the ul element based on the children index of the block
-    moveInstrumentation(block.children[index], ul);
+  // ulEls.forEach((ul, index) => {
+  //   // move the instrumentation to the ul element based on the children index of the block
+  //   moveInstrumentation(block.children[index], ul);
 
-    ul.addEventListener('mouseenter', () => {
-      ul.classList.add('active');
-    });
-    ul.addEventListener('mouseleave', () => {
-      ul.classList.remove('active');
-    });
-  });
+  //   ul.addEventListener('mouseenter', () => {
+  //     ul.classList.add('active');
+  //   });
+  //   ul.addEventListener('mouseleave', () => {
+  //     ul.classList.remove('active');
+  //   });
+  // });
 
-  block.textContent = '';
-  block.append(...ulEls);
+  // block.textContent = '';
+  // block.append(...ulEls);
 }
