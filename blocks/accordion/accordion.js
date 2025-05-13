ed12e42ea3a938a72998ad8832dee15c1493f7f2
Ben Helleman <bhellema@adobe.com>
2025-05-13 10:20:13 -0400

    test

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,5 +1,3 @@
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
