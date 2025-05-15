1d10d925413f2d58b23f5ddd575d488bee51be95
Ben Helleman <bhellema@adobe.com>
2025-05-15 13:39:48 -0400

    clean up

--- templates/personal/personal.js
+++ templates/personal/personal.js
@@ -63,6 +63,11 @@ function addHeroToPage(fragment, fragmentId) {
   if (heroWrapper) {
     heroWrapper.setAttribute('data-cc-page', fragmentId);
     heroContainer.appendChild(heroWrapper);
+
+  }
+  const container = fragment.querySelector('.hero-container');
+  if (container) {
+    container.remove();
   }
   return heroWrapper;
 }
