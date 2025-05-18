753ef5c9751c6d82d4cf6a4a339242818de8268b
Ben Helleman <bhellema@adobe.com>
2025-05-18 19:42:45 -0400

    cc

--- templates/creditcards/creditcards.js
+++ templates/creditcards/creditcards.js
@@ -6,33 +6,12 @@ export default function decorate(document) {
   const heros = document.querySelectorAll('.hero-wrapper');
   heros.forEach((hero, i) => {
     hero.dataset.tabIndex = i;
-    // add the hero to the main element at the top but after the other heros
     heroContainer.append(hero);
   });
 
   main.insertBefore(heroContainer, main.children[0]);
 
-  // move all .card-tabs-container to the .card-tabs-container
   const container = document.querySelector('.card-tabs-container');
   const panels = document.querySelectorAll('.tabs-panel');
   container.append(...panels);
-
-  document.addEventListener('card-tabs-updated', (event) => {
-    // const { tabIndex } = event.detail;
-    // const hero = document.querySelector(`.hero-wrapper[data-tab-index="${tabIndex}"]`);
-    // hero.classList.add('active');
-
-    // const cardTabsContainer = document.querySelector('.card-tabs-container');
-    // const target = document.querySelector('.tabs-panel.active > .hero-wrapper');
-
-    // move the card-tabs-container as a sibling of the target
-    // if (cardTabsContainer && target) {
-    //   //target.parentNode.insertBefore(cardTabsContainer, target.nextSibling);
-    // }
-
-    // const mainContainer = document.createElement('div');
-    // mainContainer.className = 'sectionxxxxx';
-    // mainContainer.appendChild(document.querySelector('.card-tabs-container'));
-    // document.querySelector('main').prepend(mainContainer);
-  });
 }
