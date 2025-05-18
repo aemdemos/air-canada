c952df881d41358953e8825f6e4c6d863605923c
Ben Helleman <bhellema@adobe.com>
2025-05-18 14:58:32 -0400

    support title

--- blocks/two-columns/two-columns.js
+++ blocks/two-columns/two-columns.js
@@ -6,6 +6,15 @@ export default function decorate(block) {
 
   cards.forEach((card) => {
     card.classList.add('two-columns-item');
+    // if the immediate child div does not contain a picture then add a class called title
+    if (!card.querySelector('div:first-child > picture')) {
+      card.classList.add('two-columns-title');
+      const h2 = document.createElement('h2');
+      h2.textContent = card.querySelector('div:first-child').textContent;
+      card.innerHTML = '';
+      card.querySelector('div:first-child').appendChild(h2);
+    }
+
     container.append(card);
   });
 
