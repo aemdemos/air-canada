2502e194f033cbd5ea4e1a00d392cc88abfe43a9
Ben Helleman <bhellema@adobe.com>
2025-05-18 15:08:14 -0400

    two columns

--- blocks/two-columns/two-columns.js
+++ blocks/two-columns/two-columns.js
@@ -6,8 +6,9 @@ export default function decorate(block) {
 
   cards.forEach((card) => {
     card.classList.add('two-columns-item');
-    // if the immediate child div does not contain a picture then add a class called title
-    if (!card.querySelector('div:first-child > picture')) {
+    // if the first child div contains a picture then we have no title OR
+    // if the first child is a div, with no text content then we don't have a title
+    if (card.querySelector('div:first-child > picture') === null && card.querySelector('div:first-child').textContent !== '') {
       card.classList.add('two-columns-title');
       const h2 = document.createElement('h2');
       h2.textContent = card.querySelector('div:first-child').textContent;
