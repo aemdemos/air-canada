41952bf23651c0d60ab063ba5f9616a90060aae7
Ben Helleman <bhellema@adobe.com>
2025-05-18 15:29:43 -0400

    fix h2

--- blocks/two-columns/two-columns.js
+++ blocks/two-columns/two-columns.js
@@ -6,13 +6,16 @@ export default function decorate(block) {
 
   cards.forEach((card) => {
     card.classList.add('two-columns-item');
-    // if the first child div contains a picture then we have no title OR
-    // if the first child is a div, with no text content then we don't have a title
-    if (card.querySelector('div:first-child > picture') === null && card.querySelector('div:first-child').textContent !== '') {
+    const firstDiv = card.querySelector('div:first-child');
+    const hasPicture = firstDiv.querySelector(':scope > picture') !== null;
+    const hasText = firstDiv.textContent.trim() !== '';
+
+    if (!hasPicture && hasText) {
       card.classList.add('two-columns-title');
       const h2 = document.createElement('h2');
-      h2.textContent = card.querySelector('div:first-child').textContent;
-      card.querySelector('div:first-child').append(h2);
+      h2.innerHTML = firstDiv.innerHTML;
+      firstDiv.innerHTML = '';
+      firstDiv.append(h2);
     }
     container.append(card);
   });
