50151cdfaf23716d93aa4916ea0b9509b0d09fb3
Ben Helleman <bhellema@adobe.com>
2025-05-18 23:08:21 -0400

    clean up

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,22 +1,11 @@
-// import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  // debugger;
-  // const ulEls = block.querySelectorAll(':scope div');
-
-  // ulEls.forEach((ul, index) => {
-  //   // move the instrumentation to the ul element based on the children index of the block
-  //   moveInstrumentation(block.children[index], ul);
-
-  //   ul.addEventListener('mouseenter', () => {
-  //     ul.classList.add('active');
-  //   });
-  //   ul.addEventListener('mouseleave', () => {
-  //     ul.classList.remove('active');
-  //   });
-  // });
-
-
-  // // block.textContent = '';
-  // block.append(...ulEls);
+  const ulEls = block.querySelectorAll(':scope ul');
+  ulEls.forEach((ul) => {
+    ul.addEventListener('mouseenter', () => {
+      ul.classList.add('active');
+    });
+    ul.addEventListener('mouseleave', () => {
+      ul.classList.remove('active');
+    });
+  });
 }
